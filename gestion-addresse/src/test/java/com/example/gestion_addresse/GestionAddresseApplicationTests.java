3489e9f54fb673fffc81b291b509054e23648f28
samini <soufianeamini2002@gmail.com>
2024-10-12 18:56:48 +0100

    add microservice spring boot gestion-addresse

---
+++ gestion-addresse/src/test/java/com/example/gestion_addresse/GestionAddresseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestion_addresse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionAddresseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
