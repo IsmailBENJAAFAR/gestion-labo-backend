0f392c658ea1384e81a44cdd615565671e99a13d
AMIdrissi <amidrissi2002@gmail.com>
2024-10-12 19:23:11 +0100

    thee ACTUAL commit for the spring boot stuff, not yet tested

---
+++ gestion-laboratoire/src/main/java/com/example/gestion_laboratoire/GestionLaboratoireApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestion_laboratoire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionLaboratoireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionLaboratoireApplication.class, args);
+	}
+
+}
