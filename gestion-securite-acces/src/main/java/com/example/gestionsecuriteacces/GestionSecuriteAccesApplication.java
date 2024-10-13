67b32aeeaf6bb1eb79c3ad9d47be22398c7f39da
IsmailBENJAAFAR <benjaafarismail20@gmail.com>
2024-10-13 11:38:05 +0100

    add gestion-securite-acces microservice

---
+++ gestion-securite-acces/src/main/java/com/example/gestionsecuriteacces/GestionSecuriteAccesApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestionsecuriteacces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionSecuriteAccesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionSecuriteAccesApplication.class, args);
+	}
+
+}
