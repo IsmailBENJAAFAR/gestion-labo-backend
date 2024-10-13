e71a4b8915a5d1c6fa99a0ffee4a855cac8c2927
AMIdrissi <amidrissi2002@gmail.com>
2024-10-13 14:52:48 +0100

    added last microservice >integration-services-externes< using spring boot

---
+++ integration-services-externes/src/main/java/com/example/integration_services_externes/IntegrationServicesExternesApplication.java
@@ -0,0 +1,13 @@
+package com.example.integration_services_externes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationServicesExternesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationServicesExternesApplication.class, args);
+	}
+
+}
