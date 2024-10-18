0171cfba79e9a2f8fcd9aca1c43f0fef2e3544cd
AMIdrissi <amidrissi2002@gmail.com>
2024-10-18 22:13:19 +0100

    resolved contributors requested changes (I suck at programming)

--- gestion-analyse/my_api/models/Analyse.py
+++ gestion-analyse/my_api/models/Analyse.py
@@ -1,14 +1,9 @@
 from django.db import models
 
-# from my_api.models.Laboratoire import Laboratoire
-
 # Analyse class
 class Analyse(models.Model):
     nom = models.CharField(max_length=255)
     description = models.TextField()
-    # idFkLaboratoire = models.ForeignKey(Laboratoire,
-    #                                     on_delete=models.CASCADE,
-    #                                     db_column="idFkLaboratoire",db_constraint=False)
     idFkLaboratoire = models.IntegerField(blank=False, null=False)
     
     def __str__(self):
--- gestion-analyse/my_api/urls.py
+++ gestion-analyse/my_api/urls.py
@@ -5,7 +5,4 @@ urlpatterns = [
     path("",view=analyse_views.get_all,name="Analyse-view-list"),
     path("create",view=analyse_views.create,name="Analyse-create-list"),
     path("<int:id>/",view=analyse_views.RUD_by_id,name="Analyse-view-rud"),
-    # path("Laboratoire/",view=labo_views.get_all,name="Laboratoire-view-list"),
-    # path("Laboratoire/create",view=labo_views.create,name="Laboratoire-create-list"),
-    # path("Laboratoire/<int:id>/",view=labo_views.RUD_by_id,name="Laboratoire-view-rud"),
 ]
