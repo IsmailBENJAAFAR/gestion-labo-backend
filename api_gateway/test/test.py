fff9b9fdefd30313b6a550945e40dff7278a4f69
samini <soufianeamini2002@gmail.com>
2024-10-23 21:13:53 +0100

    update test

--- api_gateway/test/test.py
+++ api_gateway/test/test.py
@@ -1,36 +1,49 @@
 from testcontainers.core.container import DockerClient, DockerContainer, wait_for_logs
 import requests
 
+caddy_test_message = "test caddy success"
+
 def generate_caddy_file():
     with open("../Caddyfile") as caddyfile:
-        text = caddyfile.read().replace("labo-sai.engineer {", ':80 {\n\trespond "test caddy success"')
+        text = caddyfile.read().replace("labo-sai.engineer {", f':80 {{\n\trespond "{caddy_test_message}"')
         with open("./Caddyfile", "w") as caddyfile_test:
             caddyfile_test.write(text)
             print("Caddyfile_test written.")
+
+def generate_dockerfile():
     with open("../Dockerfile") as dockerfile:
         text = dockerfile.read()
         with open("./Dockerfile", "w") as dockerfile_test:
             dockerfile_test.write(text)
             print("Dockerfile written")
 
-def docker_build():
+def build_image():
     client = DockerClient()
-    _, logs = client.build(path=".", tag="caddy_test")
+    client.build(path=".", tag="caddy_test")
+
+def test_caddy():
+    print("Running caddy_test container")
     with DockerContainer("caddy_test").with_exposed_ports(80) as container:
         wait_for_logs(container, "server running")
         host = f"http://{container.get_container_host_ip()}"
         port = container.get_exposed_port(80)
 
+        print("Making request to caddy_test container")
         response = requests.get(f"{host}:{port}")
         assert response.status_code == 200
+        assert response.content.decode('utf-8') == caddy_test_message
 
         logs = container.get_logs()
-        print(f"Stdout: {logs[0].decode('ascii')}")
-        print(f"Stderr: {logs[1].decode('ascii')}")
+        with open("logs.txt", "w") as logfile:
+            logfile.write(logs[0].decode('utf-8'))
+            logfile.write(logs[1].decode('utf-8'))
+    print("Test Success!")
 
 def test():
     generate_caddy_file()
-    docker_build()
+    generate_dockerfile()
+    build_image()
+    test_caddy()
 
 if __name__ == '__main__':
     test()
