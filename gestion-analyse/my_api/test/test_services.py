36fd61d67e90df8b1b767cc737a247398297523a
AMIdrissi <amidrissi2002@gmail.com>
2024-10-24 07:59:29 +0100

    some simple testing done

--- gestion-analyse/my_api/test/test_services.py
+++ gestion-analyse/my_api/test/test_services.py
@@ -2,26 +2,34 @@ from django.test import TestCase
 from testcontainers.core.container import DockerContainer
 from testcontainers.core.container import wait_for_logs
 from django.core import management
+from rest_framework import status
 
+from my_api.services.delete import delete_analyse
 from my_api.services.create import create_analyse
-from my_api.services.get import get_all
+from my_api.services.get import *
+from my_api.services.update import update_analyse
 
 
 class TestServices(TestCase):
-    with DockerContainer("postgres:16").with_env("POSTGRES_USER", "ami").with_env(
-        "POSTGRES_PASSWORD", "pwd"
-    ).with_bind_ports(5432, 5432) as postgres:
+    postgres = (
+        DockerContainer("postgres:16")
+        .with_env("POSTGRES_USER", "ami")
+        .with_env("POSTGRES_PASSWORD", "pwd")
+        .with_bind_ports(5432, 5432)
+        .start()
+    )
 
-        print("starting Container at port 5432")
+    wait_for_logs(postgres, "ready to accept connections")
+    print("Starting Container at port 5432")
+    management.call_command(
+        "migrate",
+        "my_api",
+    )
 
-        wait_for_logs(postgres, "ready to accept connections")
-        management.call_command(
-            "migrate",
-            "my_api",
-        )
+    def test_test(self):
 
-        print("Testing data creation")
-
+        # test creating data
+        print("Testing database data creation\n")
         create = create_analyse(
             data={
                 "nom": "new idea",
@@ -29,9 +37,48 @@ class TestServices(TestCase):
                 "idFkLaboratoire": 1,
             }
         )
+        self.assertEqual(create.get("response_status"), status.HTTP_200_OK)
 
-        print("\n")
-        print(get_all())
-        assert True
-        print("\n\n\n\n\n\n\n")
-        print(postgres.get_logs())
+        # test fetching data : getAll
+        print("\nTesting data fetching from database: -> All\n")
+        self.assertEqual(len(get_all().get("response_data")), 1)
+
+        # test fetching data : getById
+        print("\nTesting data fetching from database: -> by Id\n")
+        self.assertTrue(
+            get_by_id(id=1).get("response_data"),
+        )
+
+        # test updating data
+        print("\nTesting data updating in the database\n")
+        update = update_analyse(
+            data={
+                "nom": "new idea",
+                "description": "I really hate the fact that I spent 5 hours on less than 100 lines of code ._.)",
+                "idFkLaboratoire": 1,
+            },
+            id=1,
+        )
+        self.assertEqual(
+            update.get("response_status"),
+            status.HTTP_200_OK,
+        )
+
+        # test partial update
+        print("\nTesting data updating in the database\n")
+        update = update_analyse(
+            data={
+                "nom": "No idea",
+                "description": "I really hate the fact that I spent 5 hours on less than 100 lines of code ._.)",
+            },
+            id=1,
+        )
+        self.assertEqual(
+            update.get("response_status"),
+            status.HTTP_200_OK,
+        )
+
+        # test delete data
+        print("\nTesting data deletion from the database\n")
+        delete = delete_analyse(id=1)
+        self.assertEqual(delete.get("response_status"), status.HTTP_200_OK)
--- gestion-analyse/my_api/test/test_urls.py
+++ gestion-analyse/my_api/test/test_urls.py
@@ -6,10 +6,11 @@ from my_api.controllers.analyse_controller import AnalyseById, AnalyseGeneral
 
 class TestSomethhing(SimpleTestCase):
     def test_of_the_test_i_wanna_kill_my_self(self):
+        print("testing getAll and post Url\n")
         url = reverse("Analyse-list-all-create")
         self.assertEquals(resolve(url).func.view_class, AnalyseGeneral)
 
     def test_of_the_test_i_wanna_kill_my_self2(self):
+        print("testing getById, update and delete Url\n")
         url = reverse("Analyse-create-update-delete", args=[1])
-        print(resolve(url).func.view_class)
         self.assertEquals(resolve(url).func.view_class, AnalyseById)
