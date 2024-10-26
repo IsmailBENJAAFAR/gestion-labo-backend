58f5ae0fa09879ac8f35d576ec1a01b30eaadafe
samini <soufianeamini2002@gmail.com>
2024-10-26 18:05:07 +0100

    building frontend dist

--- api_gateway/action.py
+++ api_gateway/action.py
@@ -1,12 +1,26 @@
+import os, shutil
 from argparse import ArgumentParser
 from pathlib import Path
 from git import Repo
 
 def clone_frontend_repository(user: str, token: str):
     frontend_url = f"https://{user}:{token}@github.com/soufianeamini/gestion-labo-frontend.git"
-    frontend_repo = Repo.clone_from(frontend_url, "frontend")
+    Repo.clone_from(frontend_url, "frontend")
 
     assert Path("frontend").exists() and Path("frontend").is_dir()
+
+def change_dir(path: str):
+    os.chdir(path)
+    print("Changing to directory: ", os.getcwd())
+
+def build_dist():
+    change_dir("frontend")
+    assert os.system("npm run build") == 0
+    shutil.copy2("./dist", "..")
+    change_dir("..")
+    
+def build_docker_image():
+    pass
 
 # if tests are successful, build a docker image based on the version located in the VERSION file
 # The docker image needs to have a version, and the dockerfile needs to copy the dist dir from the frontend into the image
@@ -29,4 +43,6 @@ if __name__ == "__main__":
         version = version.read().strip()
         print(f"Version: {version}")
     clone_frontend_repository(user, token)
-    # build_dist()
+    build_dist()
+    build_docker_image()
+    # push_to_registry()
