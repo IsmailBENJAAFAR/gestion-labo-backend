46efc3fe8cf9a1d502fed7373d3e945f0cf6efa7
AMIdrissi <amidrissi2002@gmail.com>
2024-11-02 21:13:54 +0100

    externalized the DB credentials, should only require a .env file

--- gestion-laboratoire/src/main/java/com/example/gestion_laboratoire/GestionLaboratoireApplication.java
+++ gestion-laboratoire/src/main/java/com/example/gestion_laboratoire/GestionLaboratoireApplication.java
@@ -1,14 +1,26 @@
 package com.example.gestion_laboratoire;
 
+import java.util.Properties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import io.github.cdimascio.dotenv.Dotenv;
 
+@SpringBootApplication
 public class GestionLaboratoireApplication {
+	private static Dotenv dotenv = Dotenv.load();
 
 	public static void main(String[] args) {
-		SpringApplication.run(GestionLaboratoireApplication.class, args);
+		SpringApplication application = new SpringApplication(GestionLaboratoireApplication.class);
+
+		Properties properties = new Properties();
+		properties.put("spring.datasource.url", dotenv.get("DB_URL"));
+		properties.put("spring.datasource.username", dotenv.get("DB_USER"));
+		properties.put("spring.datasource.password", dotenv.get("DB_PWD"));
+
+		application.setDefaultProperties(properties);
+		application.run(args);
 	}
 
 }
