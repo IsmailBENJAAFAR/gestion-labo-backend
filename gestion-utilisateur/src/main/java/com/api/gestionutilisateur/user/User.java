2dd45d029588989620a72df5f39f502083ac1dc5
IsmailBENJAAFAR <benjaafarismail20@gmail.com>
2024-11-02 17:29:29 +0100

    edit user model add password field

--- gestion-utilisateur/src/main/java/com/api/gestionutilisateur/user/User.java
+++ gestion-utilisateur/src/main/java/com/api/gestionutilisateur/user/User.java
@@ -5,6 +5,7 @@ import com.api.gestionutilisateur.laboratoire.Laboratoire;
 public class User {
     private Integer id;
     private String email;
+    private String password;
     private Laboratoire laboratoire;
     private String nomComplet;
     private Profession profession;
