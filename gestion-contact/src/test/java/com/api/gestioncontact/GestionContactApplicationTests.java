12a46621f00bdcf749ce15771418031917c5a27f
IsmailBENJAAFAR <benjaafarismail20@gmail.com>
2024-11-03 16:21:06 +0100

    tests

--- gestion-contact/src/test/java/com/api/gestioncontact/GestionContactApplicationTests.java
+++ gestion-contact/src/test/java/com/api/gestioncontact/GestionContactApplicationTests.java
@@ -1,9 +1,7 @@
 package com.api.gestioncontact;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class GestionContactApplicationTests {
 
     @Test
