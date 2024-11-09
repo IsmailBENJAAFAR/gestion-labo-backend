2a7fb6c0eef1f71278d70c8f073264e92c969f65
AMIdrissi <amidrissi2002@gmail.com>
2024-11-09 12:45:27 +0100

    fixed dockerfile

--- gestion-laboratoire/src/main/java/com/api/gestion_laboratoire/models/Laboratoire.java
+++ gestion-laboratoire/src/main/java/com/api/gestion_laboratoire/models/Laboratoire.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
