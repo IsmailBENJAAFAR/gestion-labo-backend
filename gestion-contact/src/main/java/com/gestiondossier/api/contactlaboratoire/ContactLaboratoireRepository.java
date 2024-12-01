a9a65478f40a4f5450c2844851647ad762602307
IsmailBENJAAFAR <benjaafarismail20@gmail.com>
2024-12-01 22:16:48 +0100

    Add ContactLaboratoire CRUD

--- gestion-contact/src/main/java/com/gestiondossier/api/contactlaboratoire/ContactLaboratoireRepository.java
+++ gestion-contact/src/main/java/com/gestiondossier/api/contactlaboratoire/ContactLaboratoireRepository.java
@@ -1,4 +1,9 @@
 package com.gestiondossier.api.contactlaboratoire;
 
-public interface ContactLaboratoireRepository {
+import com.gestiondossier.api.contactlaboratoire.models.entity.ContactLaboratoire;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContactLaboratoireRepository extends JpaRepository<ContactLaboratoire, Integer> {
 }
