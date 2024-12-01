a551fd006cac53c7355cf9cfebb59bb863ceec35
IsmailBENJAAFAR <benjaafarismail20@gmail.com>
2024-12-01 17:35:14 +0100

    initial commit

--- gestion-contact/src/main/java/com/gestiondossier/api/GestionContactApplication.java
+++ gestion-contact/src/main/java/com/gestiondossier/api/GestionContactApplication.java
@@ -1,4 +1,4 @@
-package com.api.gestioncontact;
+package com.gestiondossier.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GestionContactApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GestionContactApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(GestionContactApplication.class, args);
+	}
 
 }
