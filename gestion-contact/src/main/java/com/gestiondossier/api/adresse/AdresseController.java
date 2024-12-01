cf84bef428edadd11939198f6fb60ebcd5c66193
IsmailBENJAAFAR <benjaafarismail20@gmail.com>
2024-12-01 22:16:27 +0100

    Add Adresse CRUD

--- gestion-contact/src/main/java/com/gestiondossier/api/adresse/AdresseController.java
+++ gestion-contact/src/main/java/com/gestiondossier/api/adresse/AdresseController.java
@@ -1,4 +1,44 @@
 package com.gestiondossier.api.adresse;
 
+import com.gestiondossier.api.adresse.models.entity.Adresse;
+import com.gestiondossier.api.adresse.models.error.AdresseNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/v1/adresses")
+@RequiredArgsConstructor
 public class AdresseController {
+    private final AdresseService adresseService;
+
+    @GetMapping
+    public List<Adresse> findAll() {
+        return adresseService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Adresse findById(@PathVariable("id") Integer adresseId) {
+        return Optional.ofNullable(adresseService.findById(adresseId)).orElseThrow(AdresseNotFoundException::new);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Adresse createAdresse(@RequestBody Adresse adresse) {
+        return adresseService.createAdresse(adresse);
+    }
+
+    @PutMapping("/{id}")
+    public Adresse updateAdresse(@PathVariable("id") Integer id, @RequestBody Adresse adresse) {
+        return Optional.ofNullable(adresseService.updateAdresse(id, adresse)).orElseThrow(AdresseNotFoundException::new);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void deleteAdresse(@PathVariable("id") Integer adresseId) {
+        adresseService.deleteAdresse(adresseId);
+    }
 }
--- gestion-contact/src/main/java/com/gestiondossier/api/adresse/AdresseRepository.java
+++ gestion-contact/src/main/java/com/gestiondossier/api/adresse/AdresseRepository.java
@@ -1,4 +1,9 @@
 package com.gestiondossier.api.adresse;
 
-public interface AdresseRepository {
+import com.gestiondossier.api.adresse.models.entity.Adresse;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdresseRepository extends JpaRepository<Adresse, Integer> {
 }
