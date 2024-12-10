6e37bd4c5d3bdd19723869b4f0d10d98012d4f51
AMIdrissi <amidrissi2002@gmail.com>
2024-12-10 23:51:05 +0100

    analyse service savepoint 2

--- gestion-analyse/src/main/java/com/api/analyse/models/Analyse.java
+++ gestion-analyse/src/main/java/com/api/analyse/models/Analyse.java
@@ -1,12 +1,12 @@
 package com.api.analyse.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import java.time.Instant;
 import java.util.Date;
 
 @Entity
@@ -17,9 +17,12 @@ public class Analyse {
     @Id
     @SequenceGenerator(name = "analyse_sequence", sequenceName = "analyse_sequence", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "analyse_sequence")
+    @NotEmpty
     private Long id;
+    @NotEmpty
     private String nom;
     private String description;
+    @NotEmpty
     private Long fkIdLaboratoire;
     @CreationTimestamp
     private Date CreatedAt;
--- gestion-analyse/src/main/java/com/api/analyse/services/AnalyseService.java
+++ gestion-analyse/src/main/java/com/api/analyse/services/AnalyseService.java
@@ -1,13 +1,16 @@
 package com.api.analyse.services;
 
+import com.api.analyse.errors.ApiResponse;
 import com.api.analyse.models.Analyse;
 import com.api.analyse.repositores.AnalyseRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
-import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,10 +19,11 @@ import java.util.Optional;
 public class AnalyseService {
 
     private final AnalyseRepository analyseRepository;
-    private final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+    private final Validator validator;
 
     public AnalyseService(AnalyseRepository analyseRepository) {
         this.analyseRepository = analyseRepository;
+        this.validator = Validation.buildDefaultValidatorFactory().getValidator();
     }
 
     public List<Analyse> getAnalyses() {
@@ -28,23 +32,58 @@ public class AnalyseService {
 
     public Analyse getAnalyseById(Long id) {
         Optional<Analyse> analyse = analyseRepository.findById(id);
-        if(analyse.isPresent()) {
+        if (analyse.isPresent()) {
             return analyse.get();
-        }else{
+        } else {
             throw new EntityNotFoundException("Analyse not found");
         }
     }
 
     public ResponseEntity<Object> createAnalyse(Analyse analyse) {
-//        if(validator.validate(analyse)) {}
-        return null;
+        if (validator.validate(analyse).isEmpty()) {
+            return new ResponseEntity<>(new ApiResponse("Invalid request"), HttpStatus.BAD_REQUEST);
+        }
+        try {
+            analyseRepository.save(analyse);
+            return new ResponseEntity<>(new ApiResponse("Analyse created successfully"),
+                    HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ApiResponse("There has been an error when creating this analyse"),
+                    HttpStatus.BAD_REQUEST);
+        }
     }
 
-    public ResponseEntity<Object> updateAnalyse(Long id ,Analyse analyse) {
-        return null;
+    @Transactional
+    public ResponseEntity<Object> updateAnalyse(Long id, Analyse analyse) {
+        if (validator.validate(analyse).isEmpty()) {
+            return new ResponseEntity<>(new ApiResponse("Invalid request"), HttpStatus.BAD_REQUEST);
+        }
+        Optional<Analyse> analyseOpt = analyseRepository.findById(id);
+        if (analyseOpt.isPresent()) {
+            Analyse analyseOld = analyseOpt.get();
+            if (analyse.getNom() != null && !analyse.getNom().isEmpty()) {
+                analyseOld.setNom(analyse.getNom());
+            }
+            if (analyse.getDescription() != null) {
+                analyseOld.setDescription(analyse.getDescription());
+            }
+            if (analyse.getFkIdLaboratoire() != null) {
+                // needs check here for foreign key
+                analyseOld.setFkIdLaboratoire(analyse.getFkIdLaboratoire());
+            }
+            return new ResponseEntity<>(new ApiResponse("Analyse updated successfully"), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(new ApiResponse("Analyse not found"), HttpStatus.NOT_FOUND);
+        }
     }
 
-    public ResponseEntity<Object> deleteAnalyse(Long id){
-        return null;
+    public ResponseEntity<Object> deleteAnalyse(Long id) {
+        Optional<Analyse> analyse = analyseRepository.findById(id);
+        if (analyse.isPresent()) {
+            analyseRepository.delete(analyse.get());
+            return new ResponseEntity<>(new ApiResponse("Analyse deleted successfully"), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(new ApiResponse("Analyse not found"), HttpStatus.NOT_FOUND);
+        }
     }
 }
