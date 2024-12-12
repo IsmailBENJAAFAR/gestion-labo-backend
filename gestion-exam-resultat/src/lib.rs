88124c316c1231f37fc881470360ac4f9f9cd093
samini <soufianeamini2002@gmail.com>
2024-12-12 16:44:49 +0100

    refactor code into lib to be able to test controller

--- gestion-exam-resultat/src/lib.rs
+++ gestion-exam-resultat/src/lib.rs
@@ -1,5 +1,84 @@
-pub mod dao;
-pub mod exam;
+use anyhow::Context;
+use axum::{
+    extract::{FromRef, OriginalUri},
+    http::StatusCode,
+    response::IntoResponse,
+    routing::get,
+    Router,
+};
+use dotenvy::dotenv;
+use exam::dao::ExamDao;
+use sqlx::postgres::PgPoolOptions;
+use std::sync::Arc;
+use tokio::net::TcpListener;
+use tower_http::cors::CorsLayer;
+
+mod dao;
+mod exam;
+
+#[derive(Clone)]
+pub struct AppState {
+    pub exam_service: Arc<exam::service::Service>,
+}
+
+impl FromRef<AppState> for Arc<exam::service::Service> {
+    fn from_ref(input: &AppState) -> Self {
+        input.exam_service.clone()
+    }
+}
+
+pub async fn run_app() -> anyhow::Result<()> {
+    tracing_subscriber::fmt().init();
+
+    match dotenv() {
+        Ok(_) => eprintln!(".env file loaded successfully."),
+        Err(_) => eprintln!("Warning: .env file not found."),
+    };
+    let url = std::env::var("DATABASE_URL").context("Please set DATABASE_URL for database")?;
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&url)
+        .await
+        .context("can't connect to database")?;
+
+    let exam_dao = ExamDao::new(pool);
+    let exam_service = Arc::new(exam::service::Service::new(Arc::new(exam_dao)));
+    let state = AppState { exam_service };
+
+    let app = app(state);
+
+    let listener = TcpListener::bind("0.0.0.0:8080")
+        .await
+        .context("Binding listener to address")?;
+    axum::serve(listener, app).await?;
+    Ok(())
+}
+
+pub fn app(state: AppState) -> Router {
+    Router::new()
+        .fallback(handler_404)
+        .nest(
+            "/api/v1",
+            Router::new()
+                .route(
+                    "/examens",
+                    get(exam::controller::get_exams).post(exam::controller::create_exam),
+                )
+                .route(
+                    "/examens/:id",
+                    get(exam::controller::get_exam).delete(exam::controller::delete_exam),
+                ),
+        )
+        .layer(CorsLayer::permissive())
+        .with_state(state)
+}
+
+async fn handler_404(OriginalUri(uri): OriginalUri) -> impl IntoResponse {
+    (
+        StatusCode::NOT_FOUND,
+        format!("no resource found in {uri:?}"),
+    )
+}
 
 #[cfg(test)]
 mod test {
@@ -20,6 +99,11 @@ mod test {
         dao::interface::{Dao, MockDao},
         exam::{api_error::ApiError, dao::ExamDao, dto::ExamDto, model::Exam, service},
     };
+
+    #[tokio::test]
+    async fn test_exam_controller() -> Result<()> {
+        Ok(())
+    }
 
     #[tokio::test]
     async fn test_exam_database() -> Result<()> {
