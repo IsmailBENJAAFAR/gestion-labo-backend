68407fd1492cef3c633f36811c707b24df9c1cb7
IsmailBENJAAFAR <benjaafarismail20@gmail.com>
2024-12-26 21:05:46 +0100

    update routes

--- gestion-dossier/src/main/java/com/gestiondossier/api/patient/PatientController.java
+++ gestion-dossier/src/main/java/com/gestiondossier/api/patient/PatientController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/patients")
+@RequestMapping("/api/v1/patients")
 @RequiredArgsConstructor
 public class PatientController {
 
