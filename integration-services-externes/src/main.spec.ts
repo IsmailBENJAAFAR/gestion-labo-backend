ba1da85c44306de24a8e026bea6d8ce5a207e97e
AMIdrissi <amidrissi2002@gmail.com>
2024-12-31 22:14:08 +0100

    yeeted one of the tests as it's...e2e idk how I missed that

--- integration-services-externes/src/main.spec.ts
+++ integration-services-externes/src/main.spec.ts
@@ -1,35 +1,35 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './app.module';
-import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
+// import { Test, TestingModule } from '@nestjs/testing';
+// import { INestApplication } from '@nestjs/common';
+// import * as request from 'supertest';
+// import { AppModule } from './app.module';
+// import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
 
-describe('Main (e2e)', () => {
-  let app: INestApplication<any>;
+// describe('Main (e2e) <- idk how I did not even see this', () => {
+//   let app: INestApplication<any>;
 
-  beforeAll(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-      providers: [
-        {
-          provide: AmqpConnection,
-          useValue: {
-            managedConnection: jest.fn(),
-          },
-        },
-      ],
-    }).compile();
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+//   beforeAll(async () => {
+//     const moduleFixture: TestingModule = await Test.createTestingModule({
+//       imports: [AppModule],
+//       providers: [
+//         {
+//           provide: AmqpConnection,
+//           useValue: {
+//             managedConnection: jest.fn(),
+//           },
+//         },
+//       ],
+//     }).compile();
+//     app = moduleFixture.createNestApplication();
+//     await app.init();
+//   });
 
-  it('should start the application and listen on port 3000', async () => {
-    const response = await request(app.getHttpServer()).get('');
-    expect(response.text).toEqual('blyat');
-    expect(response.status).not.toEqual(404);
-  });
+//   it('should start the application and listen on port 3000', async () => {
+//     const response = await request(app.getHttpServer()).get('');
+//     expect(response.text).toEqual('blyat');
+//     expect(response.status).not.toEqual(404);
+//   });
 
-  afterAll(async () => {
-    await app.close();
-  });
-});
+//   afterAll(async () => {
+//     await app.close();
+//   });
+// });
