f0203c422f9dd102237b4a801279ba3a27437327
IsmailBENJAAFAR <benjaafarismail20@gmail.com>
2025-01-01 00:14:49 +0100

    update

--- gestion-utilisateur/src/main/java/com/gestionutilisateur/api/book/BookRepository.java
+++
@@ -1,6 +0,0 @@
-package com.gestionutilisateur.api.book;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Integer> {
-}
