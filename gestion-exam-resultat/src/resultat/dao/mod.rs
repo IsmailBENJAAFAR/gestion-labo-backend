4fbd4dbdd7a9344780e2116aaef6da1e53d4d3ab
samini <soufianeamini2002@gmail.com>
2025-01-03 17:57:54 +0100

    implement dao for resultat

--- gestion-exam-resultat/src/resultat/dao/mod.rs
+++ gestion-exam-resultat/src/resultat/dao/mod.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use axum::async_trait;
+use sqlx::Row;
 use sqlx::{Pool, Postgres};
 
 use crate::dao::interface::Dao;
@@ -19,19 +20,58 @@ impl ResultatDao {
 
 #[async_trait]
 impl Dao<Resultat> for ResultatDao {
+    async fn find(&self, id: i32) -> Result<Resultat> {
+        let res = sqlx::query("SELECT * from resultat WHERE id = $1")
+            .bind(id)
+            .fetch_one(&self.pool)
+            .await?;
+
+        let resultat = Resultat::try_from(res)?;
+
+        Ok(resultat)
+    }
     async fn insert(&self, data: &Resultat) -> Result<i32> {
-        todo!("implement insert resultat")
+        let res = sqlx::query("INSERT INTO resultat (fk_id_exam, observation, score, created_at) VALUES ($1, $2, $3, $4) RETURNING id")
+            .bind(data.fk_id_exam)
+            .bind(&data.observation)
+            .bind(data.score)
+            .bind(data.created_at)
+            .fetch_one(&self.pool)
+            .await?;
+        let id: i32 = res.try_get("id")?;
+        Ok(id)
     }
     async fn update(&self, data: &Resultat) -> Result<Resultat> {
-        todo!("implement update resultat")
+        let updated_at = chrono::Utc::now();
+        let res = sqlx::query("UPDATE resultat SET observation = $1, score = $2, updated_at = $3 WHERE id = $4 RETURNING *")
+            .bind(&data.observation)
+            .bind(data.score)
+            .bind(updated_at)
+            .bind(data.id)
+            .fetch_one(&self.pool)
+            .await?;
+
+        let resultat = Resultat::try_from(res)?;
+        Ok(resultat)
     }
+
     async fn remove(&self, id: i32) -> Result<bool> {
-        todo!("implement remove resultat")
-    }
-    async fn find(&self, id: i32) -> Result<Resultat> {
-        todo!("implement find resultat")
+        let res = sqlx::query("DELETE FROM resultat WHERE id = $1")
+            .bind(id)
+            .execute(&self.pool)
+            .await?;
+
+        Ok(res.rows_affected() == 1)
     }
     async fn find_all(&self) -> Result<Vec<Resultat>> {
-        todo!("implement find_all resultats")
+        let res = sqlx::query("SELECT * FROM resultat")
+            .fetch_all(&self.pool)
+            .await?;
+        let mut resultats: Vec<Resultat> = Vec::new();
+        for entry in res {
+            resultats.push(Resultat::try_from(entry)?);
+        }
+
+        Ok(resultats)
     }
 }
