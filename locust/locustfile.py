0ae3095c8e20bb52c040a52bb76a568272e79d30
samini <soufianeamini2002@gmail.com>
2025-01-05 23:18:36 +0100

    locust

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -3,12 +3,11 @@ from locust import HttpUser, task
 class GestionLaboUser(HttpUser):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        with open("url.txt") as url_file:
-            self.baseUrl = url_file.read().strip()
-
-    def url(self, url: str):
-        return f"{self.baseUrl}/{url}"
 
     @task
     def connect(self):
-        self.client.get(self.url("/dashboard"))
+        self.client.get("/dashboard")
+
+    @task
+    def examens(self):
+        self.client.get("/dashboard/examen")
