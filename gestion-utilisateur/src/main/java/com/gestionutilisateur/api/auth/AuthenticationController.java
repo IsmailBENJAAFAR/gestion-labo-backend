ac296fdbfd25396e6131fff46fae2178aa6fad6c
AMIdrissi <amidrissi2002@gmail.com>
2025-01-05 11:14:49 +0100

    some changes for auth in the back

--- gestion-utilisateur/src/main/java/com/gestionutilisateur/api/auth/AuthenticationController.java
+++ gestion-utilisateur/src/main/java/com/gestionutilisateur/api/auth/AuthenticationController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import java.io.IOException;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@CrossOrigin
 public class AuthenticationController {
 
     private final AuthenticationService service;
